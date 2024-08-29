f339fa87c781aad622180fc08a0af935fff8955b
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-08-29 20:49:39 -0300

    Update leap_year.py

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,7 +1,8 @@
 def is_leap_year() -> bool:
     año: int = int(input("Ingrese un año: "))
-    bisiesto: bool = False
     if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
-        bisiesto = True
-    print(f"El año {año} {'es' if bisiesto else 'no es'} bisiesto")
+        bisiesto: bool = True
+    else:
+        bisiesto: bool = False
+    print(f"El año {año} {'es bisiesto' if bisiesto else 'no es bisiesto'}")
     return bisiesto
