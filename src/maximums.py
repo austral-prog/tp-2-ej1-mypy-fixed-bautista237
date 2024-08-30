88914dc07111f2cd99699dcc9470048c43fc1b9c
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-08-29 21:54:48 -0300

    Update maximums.py

--- src/maximums.py
+++ src/maximums.py
@@ -1,15 +1,14 @@
 def max_of_two(x: float, y: float) -> float:
     """Given x and y, that are 2 numbers, return the biggest number."""
     if x >= y:
-        biggest = x  
+        biggest = x  # Solo se asigna el valor sin la declaración de tipo
     else:
         biggest = y
     return biggest
-max_of_two(5,4):
 
 def max_of_three(x: float, y: float, z: float) -> float:
     """Given x, y, and z, which are 3 numbers, return the biggest number of the three."""
-    max_value = x 
+    max_value = x  # Solo se asigna el valor sin la declaración de tipo
 
     if y > max_value:
         max_value = y
@@ -17,4 +16,3 @@ def max_of_three(x: float, y: float, z: float) -> float:
         max_value = z
 
     return max_value
-max_of_three(5,4,7):
