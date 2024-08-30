b1e909d36adb632fc692704a51f78f3292651b66
bautista237 <162723056+bautista237@users.noreply.github.com>
2024-08-29 22:37:25 -0300

    Update leap_year.py

--- src/leap_year.py
+++ src/leap_year.py
@@ -1,4 +1,4 @@
-def is_leap_year() -> bool:
+def leap_year() -> bool:
     año: int = int(input("Ingrese un año: "))
     # Determina si el año es bisiesto
     if (año % 4 == 0 and año % 100 != 0) or (año % 400 == 0):
